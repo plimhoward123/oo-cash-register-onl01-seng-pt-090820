f292af058614c4e75ada7a593f00322af87cb174
Plimon Howard <plimon.howard@gmail.com>
2020-10-09 19:04:29 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,11 +1,11 @@
 class CashRegister
   attr_accessor :dscnt, :total
-  new(val) = (total = 0, initialize(val))
+  @@new(val) = (total = 0, initialize(val))
 
   def initialize(discout = 0)
     @dscnt = discount
   end
   def total
-    @total
+    return @total
   end
 end
