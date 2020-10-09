d6ca522b49c9bb144e5048b8415c60d0ead4cd92
Plimon Howard <plimon.howard@gmail.com>
2020-10-09 19:34:43 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,14 +1,20 @@
 class CashRegister
-  attr_accessor :discount, :total
+  attr_accessor :discount, :total, 
 
   def initialize(discout = 0)
     self.new(discout)
   end
+  
   def new(dis = 0)
     self.discount = dis
     self.total = 0
   end
+  
   def total
      @total
+  end
+
+  def add_item(item,price,quant = 1)
+    self.total = self.total + price
   end
 end
